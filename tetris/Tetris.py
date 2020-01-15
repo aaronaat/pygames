ca75601f7cd041f6ae8e1210932cb2ea151db4a0
at <at>
2020-01-14 21:52:50 -0800

    check valid space

--- tetris/Tetris.py
+++ tetris/Tetris.py
@@ -170,8 +170,24 @@ def convert_shape_format(shape):
         positions[i] = (pos[0] - 2, pos[1] - 4)
  
 def valid_space(shape, grid):
-    pass
- 
+
+    ##   Convert the list to one dimension.
+    ###
+    ####
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
+    accepted_pos = [j for sub in accepted_pos for j in sub]
+    #####
+    ####   ie; [[(0,1)], [(2,3)]]  -->  [(0,1), (2,3)]
+    ###
+    formatted = convert_shape_format(shape)
+    
+    for pos in formatted:
+        if_pos not in accepted_pos:
+            if pos[1] > -1:
+                return False
+        return True
+
+
 def check_lost(positions):
     pass
  
